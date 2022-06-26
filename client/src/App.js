fd6c28d4536cffa3a9d000abb789a71fb231c34d
D-izzy-G <donovangalloway209@gmail.com>
2022-06-25 18:14:04 -0700

    stateful counter

---
+++ client/src/App.js
@@ -0,0 +1,80 @@
+import {useState,useEffect} from 'react'
+
+import './App.css';
+import axios from 'axios'
+
+
+
+function App() {
+  const [value, setValue] = useState(0)
+  
+  const [posts, setPosts] = useState([])
+
+
+  useEffect(()=>{
+    getValue()
+   
+  },[])
+
+  const getValue = () => {
+    axios.get('http://localhost:5000/myBackend')
+      .then((response) => {
+        const data = response.data
+        setPosts(data)
+        console.log(data)
+        if(data.length){
+
+          setValue(data[data.length - 1].body)
+        }
+        console.log('We have received your data')
+      })
+      .catch(()=>{
+        alert('Error retrieving data')
+      })
+  }
+  
+
+
+
+  const submit= (e) => {
+    e.preventDefault()
+
+    const payload ={
+      body: value
+    }
+
+    axios({
+      url: 'http://localhost:5000/save',
+      method: 'POST',
+      data: payload
+    })
+    .then(()=>{
+      console.log('Data has been sent to the server')
+    })
+    .catch(()=>{
+      console.log('Internal server error')
+    })
+  }
+
+
+  
+  return (
+    
+    <div className="App">
+        {value}
+      <button onClick={()=>{setValue(value + 1)}}>Increment</button>
+      <button onClick={()=>{setValue(value - 1)}}>Decrement</button>
+      <button onClick={()=>{setValue(value - value)}
+      }>Reset</button>
+     <button onClick = {submit}>Save</button>
+
+      <div>
+      
+      </div>
+
+    </div>
+    
+  );
+}
+
+export default App;
