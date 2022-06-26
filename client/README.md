4ee19fcab42ff35ce332914d0e91c0733e94dcb5
D-izzy-G <donovangalloway209@gmail.com>
2022-06-25 18:33:42 -0700

    Added a bit of styling

---
+++ client/README.md
@@ -0,0 +1,3 @@
+A increment counter that implements a save button to save your place/ progress.
+
+Implements frontend (react), backend(node / express), server(mongodb)
--- client/src/App.js
+++ client/src/App.js
@@ -61,12 +61,24 @@ function App() {
   return (
     
     <div className="App">
-        {value}
-      <button onClick={()=>{setValue(value + 1)}}>Increment</button>
-      <button onClick={()=>{setValue(value - 1)}}>Decrement</button>
-      <button onClick={()=>{setValue(value - value)}
-      }>Reset</button>
-     <button onClick = {submit}>Save</button>
+
+        <div className="save-button">
+            <h2>Remember to save your progress!</h2>
+            <button onClick = {submit}>Save</button>
+        </div>
+
+        <div className = "Value">
+            {value}
+        </div>
+
+        <div>
+            <button onClick={()=>{setValue(value + 1)}}>Increment</button>
+  
+            <button onClick={()=>{setValue(value - 1)}}>Decrement</button>
+        
+            <button onClick={()=>{setValue(value - value)}}>Reset</button>
+        </div>
+     
 
       <div>
       
